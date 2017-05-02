b8c738269c975a645932d6276da80d90349026a0
Vaibhav Chahal <vaibhav.chahal@tothenew.com>
2017-05-02 13:21:26 +0530

    added Baseactivity.

---
+++ app/src/main/java/com/kokaihop/utility/BaseActivity.java
@@ -0,0 +1,12 @@
+package com.kokaihop.utility;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Vaibhav Chahal on 2/5/17.
+ */
+
+public class BaseActivity extends AppCompatActivity{
+
+    // TODO: base feature of the app.
+}
