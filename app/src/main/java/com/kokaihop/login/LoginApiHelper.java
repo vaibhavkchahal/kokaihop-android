f6a83d1422e925e05ed16812fec242098a25553f
Vaibhav Chahal <vaibhav.chahal@tothenew.com>
2017-05-03 18:03:03 +0530

    created retrofit client

---
+++ app/src/main/java/com/kokaihop/login/LoginApiHelper.java
@@ -0,0 +1,39 @@
+package com.kokaihop.login;
+
+import android.content.Context;
+
+import com.kokaihop.network.RetrofitClient;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+/**
+ * Created by Vaibhav Chahal on 3/5/17.
+ */
+
+public class LoginApiHelper {
+    private Context context;
+
+    public LoginApiHelper(Context context) {
+        this.context = context;
+    }
+
+    public void doLogin(String email,String password){
+        LoginApiInterface loginApiInterface = RetrofitClient.getInstance().create(LoginApiInterface.class);
+        Call<LoginApiResponse> loginApiResponseCall = loginApiInterface.doLogin(email,password);
+
+        loginApiResponseCall.enqueue(new Callback<LoginApiResponse>() {
+            @Override
+            public void onResponse(Call<LoginApiResponse> call, Response<LoginApiResponse> response) {
+
+            }
+
+            @Override
+            public void onFailure(Call<LoginApiResponse> call, Throwable t) {
+
+            }
+        });
+    }
+
+}
