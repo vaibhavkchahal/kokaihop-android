692f24a874baeebc72033fe9ed20531dc52eb371
Bhupendra Joshi <bhupendra.joshi@tothenew.com>
2017-05-09 10:37:19 +0530

    layout for select city created (Relative Layout).

---
+++ app/src/main/java/com/kokaihop/selectcity/SelectCityActivity.java
@@ -0,0 +1,15 @@
+package com.kokaihop.selectcity;
+
+import android.os.Bundle;
+
+import com.altaworks.kokaihop.ui.R;
+import com.kokaihop.utility.BaseActivity;
+
+public class SelectCityActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_city);
+    }
+}
