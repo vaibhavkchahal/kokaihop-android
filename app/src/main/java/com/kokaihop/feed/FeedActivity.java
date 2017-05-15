3811d3abe6448873ed4d521fed133295caec0709
Vaibhav Chahal <vaibhav.chahal@tothenew.com>
2017-05-15 13:52:25 +0530

    added tabs with pager in fragment feed.

--- app/src/main/java/com/kokaihop/feed/FeedActivity.java
+++ app/src/main/java/com/kokaihop/feed/FeedActivity.java
@@ -1,9 +1,6 @@
 package com.kokaihop.feed;
 
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
 
 import com.altaworks.kokaihop.ui.R;
@@ -15,20 +12,9 @@ public class FeedActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tablayout_recipe);
-        tabLayout.addTab(tabLayout.newTab().setText("MAIN COURSE OF THE DAY"));
-        tabLayout.addTab(tabLayout.newTab().setText("APPETIZER OF THE DAY"));
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 3"));
-        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
-
-        final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
-        final PagerAdapter adapter = new RecipeTabAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
-        viewPager.setAdapter(adapter);
-        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-
     }
 
 }
