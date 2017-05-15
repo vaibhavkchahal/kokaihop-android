7a8512025faad3071666ca9ed2c49f2f9929d776
Bhupendra Joshi <bhupendra.joshi@tothenew.com>
2017-05-15 10:18:16 +0530

    EditText properties changed.

--- app/src/main/java/com/kokaihop/utility/BindingUtils.java
+++ app/src/main/java/com/kokaihop/utility/BindingUtils.java
@@ -1,6 +1,7 @@
 package com.kokaihop.utility;
 
 import android.databinding.BindingAdapter;
+import android.support.design.widget.TextInputLayout;
 import android.widget.TextView;
 
 /**
@@ -12,5 +13,10 @@ public class BindingUtils {
     @BindingAdapter({"app:font"})
     public static void setFont(TextView textView, String fontName) {
         textView.setTypeface(CustomFontFamily.getInstance().getFont(fontName));
+    }
+
+    @BindingAdapter({"app:font"})
+    public static void setFont(TextInputLayout textInputLayout, String fontName) {
+        textInputLayout.setTypeface(CustomFontFamily.getInstance().getFont(fontName));
     }
 }
