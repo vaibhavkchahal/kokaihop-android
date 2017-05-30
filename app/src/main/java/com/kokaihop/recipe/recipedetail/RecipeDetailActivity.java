b1d2bcc576623989f9035a9d37d7cc089d11da10
Vaibhav Chahal <vaibhav.chahal@tothenew.com>
2017-05-30 12:43:08 +0530

    created recipe detail activity.

---
+++ app/src/main/java/com/kokaihop/recipe/recipedetail/RecipeDetailActivity.java
@@ -0,0 +1,15 @@
+package com.kokaihop.recipe.recipedetail;
+
+import android.os.Bundle;
+
+import com.altaworks.kokaihop.ui.R;
+import com.kokaihop.base.BaseActivity;
+
+public class RecipeDetailActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_detail);
+    }
+}
