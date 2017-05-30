198e70f6bb1d4e238cf8dcb053530cb7bc9b09d9
Vaibhav Chahal <vaibhav.chahal@tothenew.com>
2017-05-30 14:08:42 +0530

    adding viewpager in recipe detail.

--- app/src/main/java/com/kokaihop/recipe/recipedetail/RecipeDetailActivity.java
+++ app/src/main/java/com/kokaihop/recipe/recipedetail/RecipeDetailActivity.java
@@ -1,6 +1,7 @@
 package com.kokaihop.recipe.recipedetail;
 
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 
 import com.altaworks.kokaihop.ui.R;
 import com.kokaihop.base.BaseActivity;
@@ -11,5 +12,8 @@ public class RecipeDetailActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_detail);
+
+        ViewPager viewPager= (ViewPager) findViewById(R.id.viewpager_recipe_detail);
+        viewPager.setAdapter(new RecipeDetailPagerAdapter(this));
     }
 }
---
+++ app/src/main/java/com/kokaihop/recipe/recipedetail/RecipeDetailPagerAdapter.java
@@ -0,0 +1,51 @@
+package com.kokaihop.recipe.recipedetail;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.altaworks.kokaihop.ui.R;
+
+/**
+ * Created by Vaibhav Chahal on 30/5/17.
+ */
+
+public class RecipeDetailPagerAdapter extends PagerAdapter {
+
+    private Context mContext;
+    private LayoutInflater mLayoutInflater;
+    private int[] images = new int[]{R.drawable.ic_recipeplaceholder_sm, R.drawable.ic_avtar_lg, R.drawable.ic_list_orange_sm};
+
+    public RecipeDetailPagerAdapter(Context context) {
+        mContext = context;
+        mLayoutInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == ((LinearLayout) object);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        View itemView = mLayoutInflater.inflate(R.layout.recipe_detai_pager_item, container, false);
+        ImageView imageView = (ImageView) itemView.findViewById(R.id.imageview_recipe_pic);
+        imageView.setImageResource(images[position]);
+        container.addView(itemView);
+        return itemView;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((LinearLayout) object);
+    }
+}
