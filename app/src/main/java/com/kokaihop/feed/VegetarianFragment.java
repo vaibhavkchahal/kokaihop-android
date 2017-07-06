f0df8011e3d49475ce0994aad8af4c60a2d4d7eb
Vaibhav Chahal <vaibhav.chahal@tothenew.com>
2017-07-06 11:50:38 +0530

    formated date as hour,day,month ago in home tab comments.

--- app/src/main/java/com/kokaihop/feed/VegetarianFragment.java
+++ app/src/main/java/com/kokaihop/feed/VegetarianFragment.java
@@ -36,7 +36,6 @@ public class VegetarianFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
--- app/src/main/java/com/kokaihop/utility/DateTimeUtils.java
+++ app/src/main/java/com/kokaihop/utility/DateTimeUtils.java
@@ -1,6 +1,12 @@
 package com.kokaihop.utility;
 
+import android.content.Context;
+import android.text.format.DateUtils;
+
+import com.altaworks.kokaihop.ui.R;
+
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 import java.util.TimeZone;
 import java.util.concurrent.TimeUnit;
@@ -20,9 +26,50 @@ public class DateTimeUtils {
 
     public static long getOneHoursDiff(long lastUpdateTime) {
         long diffInMillisec = System.currentTimeMillis() - lastUpdateTime;
-
         long diffInHours = TimeUnit.MILLISECONDS.toHours(diffInMillisec);
         Logger.e("diffInHours", diffInHours + "");
         return diffInHours;
+    }
+
+    public static String getRelativeTimeDisplayDate(Context context, long timeStamp) {
+        String relativeTime;
+        Date serverDate = new Date(timeStamp);
+        long different = new Date().getTime() - serverDate.getTime();
+        long secondsInMilli = 1000;
+        long minutesInMilli = secondsInMilli * 60;
+        long hoursInMilli = minutesInMilli * 60;
+        long daysInMilli = hoursInMilli * 24;
+        long mDisplayDays, mDisplayHours, mDisplayMinutes;
+        mDisplayDays = different / daysInMilli;
+        different = different % daysInMilli;
+        mDisplayHours = different / hoursInMilli;
+        different = different % hoursInMilli;
+        mDisplayMinutes = different / minutesInMilli;
+        if (mDisplayDays >= 2 * 365) {
+            relativeTime = String.format(context.getResources().getString(R.string.years_ago), mDisplayDays / 365);
+        } else if (mDisplayDays >= 365 && mDisplayDays < 2 * 365) {
+            relativeTime = context.getResources().getString(R.string.year_ago);
+        } else if (mDisplayDays >= 60 && mDisplayDays < 365) {
+            relativeTime = String.format(context.getResources().getString(R.string.months_ago), mDisplayDays / 30);
+        } else if (mDisplayDays >= 30 && mDisplayDays < 60) {
+            relativeTime = context.getResources().getString(R.string.month_ago);
+        } else if (mDisplayDays >= 14 && mDisplayDays < 30) {
+            relativeTime = String.format(context.getResources().getString(R.string.weeks_ago), mDisplayDays / 7);
+        } else if (mDisplayDays >= 7 && mDisplayDays < 14) {
+            relativeTime = context.getResources().getString(R.string.week_ago);
+        } else if (mDisplayDays > 1 && mDisplayDays < 7) {
+            relativeTime = (String) DateUtils.getRelativeTimeSpanString(serverDate.getTime(), new Date().getTime(), DateUtils.DAY_IN_MILLIS);
+        } else if (mDisplayDays == 1) {
+            relativeTime = context.getResources().getString(R.string.day_ago);
+        } else if (mDisplayHours > 1) {
+            relativeTime = String.format(context.getResources().getString(R.string.hours_ago), mDisplayHours);
+        } else if (mDisplayHours == 1) {
+            relativeTime = String.format(context.getResources().getString(R.string.hour_ago), mDisplayHours);
+        } else if (mDisplayMinutes >= 1) {
+            relativeTime = String.format(context.getResources().getString(R.string.minutes_ago), mDisplayMinutes);
+        } else
+            relativeTime = context.getResources().getString(R.string.few_seconds_ago);
+//        Logger.i(TAG, "relative time: " + relativeTime);
+        return relativeTime;
     }
 }
