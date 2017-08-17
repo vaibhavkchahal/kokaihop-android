9d7ae9e6a741b6db8feed72f01e6467c0ef8eeca
Vaibhav Chahal <vaibhav.chahal@tothenew.com>
2017-08-17 14:27:30 +0530

    added corner radius on comments images.

--- app/src/main/java/com/kokaihop/utility/BindingUtils.java
+++ app/src/main/java/com/kokaihop/utility/BindingUtils.java
@@ -6,8 +6,11 @@ import android.support.design.widget.TextInputLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.altaworks.kokaihop.ui.R;
+import com.bumptech.glide.DrawableRequestBuilder;
 import com.bumptech.glide.Glide;
 import com.kokaihop.utility.glide.GlideCircularTranform;
+import com.kokaihop.utility.glide.RoundedCornersTransformation;
 
 /**
  * Created by Vaibhav Chahal on 10/5/17.
@@ -60,9 +63,12 @@ public class BindingUtils {
         }
     }
 
-    /*@BindingAdapter("android:visibility")
-    public static void setVisibility(View view, Payload model) {
-
-        view.setVisibility(model.getReplyEvents().isEmpty() ? View.GONE: View.VISIBLE);
-    }*/
+    @BindingAdapter({"app:imageUrl", "app:enableRoundedCorner", "app:radius", "app:margin"})
+    public static void loadImage(ImageView view, String url, boolean enableRoundedCorner, int radius, int margin) {
+        Logger.i("URL cloudnary-->", url);
+        DrawableRequestBuilder<Integer> thumbnail = Glide.with(view.getContext())
+                .load(R.drawable.ic_recipeplaceholder_md)
+                .bitmapTransform(new RoundedCornersTransformation(view.getContext(), radius, margin));
+        Glide.with(view.getContext()).load(url).bitmapTransform(new RoundedCornersTransformation(view.getContext(), radius, margin)).thumbnail(thumbnail).into(view);
+    }
 }
